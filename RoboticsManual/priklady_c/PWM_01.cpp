c7323bef91accde003f4df6dba4a8928c478712e
spravce <burda.miroslav@gmail.com>
2018-02-25 00:19:51 +0100

    RoboticsManual: add simple program for PWM

--- RoboticsManual/priklady_c/PWM_01.cpp
+++ RoboticsManual/priklady_c/PWM_01.cpp
@@ -2,11 +2,15 @@
 
  void setup() 
  {
+	pinMode( L_Y, OUTPUT );
 	 ledcSetup(0, 1000, 10); 
-	 ledcAttachPin(L_Y, 0);
+	    // ledcSetup(channel, freq, resolution)
+	    // channel = 0 - 15
+	    // resolution = 10 => 2^10 => 1024
+	 ledcAttachPin(L_Y, 0);  // ledcAttachPin(pin, channel)
  }
  
  void loop() 
  {
-	 ledcWrite(0, analogRead(POT1)/4);
+	 ledcWrite(0, analogRead(POT1)/4);  // potentiometer connected to analog pin POT1
  }
